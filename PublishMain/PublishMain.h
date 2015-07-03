d77745bd2fa703c89ec4d21d3610a3c025d9b5fd
Shen <polossk@126.com>
2015-07-03 22:12:26 +0800

    创建工程PublishMain
    程序的主窗口界面

---
+++ PublishMain/PublishMain.h
@@ -0,0 +1,32 @@
+
+// PublishMain.h : PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含“stdafx.h”以生成 PCH 文件"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// CPublishMainApp: 
+// 有关此类的实现，请参阅 PublishMain.cpp
+//
+
+class CPublishMainApp : public CWinApp
+{
+public:
+	CPublishMainApp();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CPublishMainApp theApp;
---
+++ PublishMain/resource.h
@@ -0,0 +1,24 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 PublishMain.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_PUBLISHMAIN_DIALOG          102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDR_MAINFRAME1                  128
+#define IDB_BITMAP1                     129
+#define IDB_BANNER                      129
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1000
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
